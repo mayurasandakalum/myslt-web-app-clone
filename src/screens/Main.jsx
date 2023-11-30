89e75a244e5c4d73bf86f5fdcd7faa274b9f00a1
mayurasandakalum <mssellapperuma@gmail.com>
2023-11-30 13:55:25 +0530

    some builds in nav bar

---
+++ src/screens/Main.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <div
+    //   className="bg-bg-main w-screen h-screen"
+    //   style={{
+    //     background: `url(${bgup}) right top no-repeat, url(${bgdown}) left bottom no-repeat`,
+    //     backgroundColor: "#0b2a58",
+    //     backgroundSize: "20%",
+    //   }}
+    >
+      Main
+    </div>
+  );
+};
+
+export default Main;
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "bg-main": "#0b2a58",
+        "bg-nav": "#0056a3",
+      },
+    },
+  },
+  plugins: [],
+};
